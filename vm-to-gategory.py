b5c5aea1d3c8fa0ad254e5887f0c19c81a899b3c
Artem Protsenko <asprocenko@gmail.com>
2021-04-09 15:25:48 +0200

    minor changes

--- vm-to-gategory.py
+++ vm-to-gategory.py
@@ -1,5 +1,5 @@
 #!/bin/python
-#Import modules and set context
+# Import modules and set context
 import json
 import getpass
 import argparse
@@ -7,11 +7,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning
-#Self-signed certificate trust handling
+# Self-signed certificate trust handling
 disable_warnings(InsecureRequestWarning)
 
-#Add handling for API reply errors
-def apicall(apiurl, apiauth, apipayload, method):
+# Add handling for API reply errors
+def apiCall(apiurl, apiauth, apipayload, method):
     try:
         if method == 'POST':
             urlData = requests.post(url = apiurl, auth=apiauth, headers=headers, data=apipayload ,verify=False)
@@ -19,6 +19,9 @@ def apicall(apiurl, apiauth, apipayload, method):
             urlData = requests.put(url = apiurl, auth=apiauth, headers=headers, data=apipayload ,verify=False)
         elif method == 'GET':
             urlData = requests.get(url = apiurl, auth=apiauth, headers=headers, verify=False)
+        else:
+            print('no method supplied. quitting...')
+            quit()
     except Exception as err:
         print(err)
     
@@ -35,35 +38,40 @@ def apicall(apiurl, apiauth, apipayload, method):
         quit()
     return urlData
 
+# Compare list of current values to new values
+def categoryValuesCompare(curValues, newValues):
+    diffValues = list(set(newValues) - set(curValues)) + list(set(curValues) - set(newValues))
+    return diffValues
+
 parser = argparse.ArgumentParser(description='''Script to add category and values to VM''', 
-                                 usage='''new.py -u user_with_admin_permissions_here -n vm_name_here -i 'PC_IP_or_FQDN_here' -c category_name_here -k category_values_here\n
+                                 usage='''new.py -u user_with_admin_permissions_here -n vmname_here -i 'PC_IP_or_FQDN_here' -c category_name_here -k category_values_here\n
                                  Category values can be supplied with \",\" as delimiter''')
-parser.add_argument('-n','--vm_name',type=str, help='Name of VM', required=True)
-parser.add_argument('-c','--category',type=str, help='Category to add.', required=True)
-parser.add_argument('-k','--category_values',type=str, help='Category values to add.', required=True)
-parser.add_argument('-i','--ip',type=str, help='Server ip or FQDN', required=True)
-parser.add_argument('-u','--user',type=str, help='User with Cluster Admin Role Mapping', required=True)
+parser.add_argument('-n', '--vmName', type=str, help='Name of VM', required=True)
+parser.add_argument('-c', '--category',type=str, help='Category to add.', required=True)
+parser.add_argument('-k', '--category_values',type=str, help='Category values to add.', required=True)
+parser.add_argument('-i', '--ip', type=str, help='Server ip or FQDN', required=True)
+parser.add_argument('-u', '--user', type=str, help='User with Cluster Admin Role Mapping', required=True)
 args = parser.parse_args()
 
-ip_addr = args.ip
-vm_name = args.vm_name
+ipAddr = args.ip
+vmName = args.vmName
 user = args.user
 categoryKey = args.category
 categoryValues = (args.category_values).split(',')
 
-apiBase = 'https://%s:9440/' % ip_addr
+apiBase = 'https://%s:9440/' % ipAddr
 headers = {'Accept': 'application/json','content-type': 'application/json',}
 auth = HTTPBasicAuth(user, getpass.getpass('Enter password for user '))
 
-#Get VM list based on VM name - in case we have multiple VMs with same name
+# Get VM list based on VM name - in case we have multiple VMs with same name
 endpointVMs = '/api/nutanix/v3/vms/list'
 listVMsPayload = '''{
   "kind": "vm",
   "filter": "vm_name==%s"
-}''' % vm_name
-jsData = json.loads(apicall(apiBase+endpointVMs, auth, listVMsPayload, 'POST').content)
+}''' % vmName
+jsData = json.loads(apiCall(apiBase + endpointVMs, auth, listVMsPayload, 'POST').content)
 
-#If more than one VM found build menu to pick VM based on UUID
+# If more than one VM found build menu to pick VM based on UUID
 menuEntries = list()
 menuIndx = list()
 selectedId = 0
@@ -79,18 +87,18 @@ if len(jsData['entities']) > 1:
 
 vmUuid = jsData['entities'][selectedId]['metadata']['uuid']
 
-#Get VM spec from PC
+# Get VM spec from PC
 endpointVM = 'api/nutanix/v3/vms/'
-jsData2 = json.loads(apicall(apiBase+endpointVM+vmUuid, auth, None, 'GET').content)
+jsData2 = json.loads(apiCall(apiBase + endpointVM + vmUuid, auth, None, 'GET').content)
 print('Processing VM: \n',jsData2['spec']['name'],jsData2['metadata']['uuid'])
 
 
-#Check if category exists and create if not
+# Check if category exists and create if not
 endpointCategory = 'api/nutanix/v3/categories/'+categoryKey
 categoryPayload = '''{
   "kind": "category"
 }'''
-categoryData = json.loads(apicall(apiBase+endpointCategory+'/list', auth, categoryPayload, 'POST').content)
+categoryData = json.loads(apiCall(apiBase + endpointCategory + '/list', auth, categoryPayload, 'POST').content)
 
 
 if not categoryData['entities'] and categoryData['metadata']['length'] ==0:
@@ -100,13 +108,13 @@ if not categoryData['entities'] and categoryData['metadata']['length'] ==0:
     "description": "created with script",
     "name": "%s"
     }''' % categoryKey
-    json.loads(apicall(apiBase+endpointCategory, auth, categoryDefinitionPayload, 'PUT').content)
+    json.loads(apiCall(apiBase + endpointCategory, auth, categoryDefinitionPayload, 'PUT').content)
     for i in categoryValues:
         categoryValueDefinitionPayload = '''{
         "value": "%s",
         "api_version": "3.1.0"
         }''' % i
-        json.loads(apicall(apiBase+endpointCategory+'/'+i, auth, categoryValueDefinitionPayload, 'PUT').content)
+        json.loads(apiCall(apiBase+endpointCategory + '/' + i, auth, categoryValueDefinitionPayload, 'PUT').content)
 else:
     existingCatVal = list()
     for i in categoryData['entities']:
@@ -119,27 +127,27 @@ else:
             "value": "%s",
             "api_version": "3.1.0"
             }''' % i
-            json.loads(apicall(apiBase+endpointCategory+'/'+i, auth, categoryValueDefinitionPayload, 'PUT').content)
+            json.loads(apiCall(apiBase + endpointCategory + '/' + i, auth, categoryValueDefinitionPayload, 'PUT').content)
 
-#Creating payload for categories
-newpayload = dict()
-newpayload['spec'] = jsData2['spec']
-newpayload['metadata'] = jsData2['metadata']
-newpayload["api_version"] = jsData2['api_version']
-newpayload['metadata']["use_categories_mapping"] = True
-currentVMcategories = newpayload['metadata']['categories_mapping']
+# Creating payload for categories
+newPayload = dict()
+newPayload['spec'] = jsData2['spec']
+newPayload['metadata'] = jsData2['metadata']
+newPayload["api_version"] = jsData2['api_version']
+newPayload['metadata']["use_categories_mapping"] = True
+curVMCategories = newPayload['metadata']['categories_mapping']
 
-#Modify/Add categories
-if len(currentVMcategories) > 0 and (categoryKey in currentVMcategories.keys()) and currentVMcategories[categoryKey].sort() == categoryValues.sort():
-    print('nothing to do')
-    quit()
-elif categoryKey in currentVMcategories.keys():
-    for j in categoryValues:
-            if not j in currentVMcategories[categoryKey]:
-                (currentVMcategories[categoryKey]).append(i)
-                continue
+# Modify/Add categories
+if len(curVMCategories) > 0 and (categoryKey in curVMCategories.keys()):
+    freshValues = categoryValuesCompare(curVMCategories[categoryKey],categoryValues)
+    if not len(freshValues) == 0:
+        for i in categoryValuesCompare(curVMCategories[categoryKey],categoryValues):
+            (curVMCategories[categoryKey]).append(i)
+    else:
+        print('nothing to add')
+        quit()
 else:
-    currentVMcategories[categoryKey] = categoryValues
+    curVMCategories[categoryKey] = categoryValues
 
-jsData3 = json.loads(apicall(apiBase+endpointVM+vmUuid, auth, json.dumps(newpayload), 'PUT').content)
-print('Task status: ',jsData3['status']['state'],'\n','Task uuid: ',jsData3['status']['execution_context']['task_uuid'])
+jsData3 = json.loads(apiCall(apiBase + endpointVM + vmUuid, auth, json.dumps(newPayload), 'PUT').content)
+print('Task status: ', jsData3['status']['state'], '\n', 'Task uuid: ', jsData3['status']['execution_context']['task_uuid'])
